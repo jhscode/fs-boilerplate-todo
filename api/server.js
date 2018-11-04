55bc785f32f6d0897b234f5ad12fa645a7e21b13
Simon Bloom <swbloom@gmail.com>
2018-11-04 10:12:47 -0500

    remove books

--- api/server.js
+++ api/server.js
@@ -10,7 +10,6 @@ app.use(bodyParser.json());
 
 app.use('/healthcheck', require('./routes/index').router);
 app.use('/login', require('./routes/login').router);
-app.use('/books', require('./routes/books').router);
 app.use('/todos', require('./routes/todos').router);
 
 app.use((err, req, res, next) => {
