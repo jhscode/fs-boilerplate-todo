801b828488ea600d4e90f55edbbd093b31b53ee7
James Shin <jhscode@gmail.com>
2018-11-11 14:56:02 -0500

    todo app showing async/await

--- api/routes/todos.js
+++ api/routes/todos.js
@@ -1,4 +1,4 @@
-const todos = []
+const todos = ['cook dinner', 'walk the dog'];
 
 const express = require('express')
 const router = express.Router()
@@ -6,20 +6,40 @@ const router = express.Router()
 router.route('/')
   .get((req, res) => {
     // 1. Respond with the array of todos
+
+    // Test to see if works
+    // 1. Check with the browser
+    // 2. Check with insomnia
+    // 3. Make the request
+    res.status(200).send({
+      data: todos
+    })
   })
 
+// POST
 router.route('/')
   .post((req, res) => {
     // 1. Grab the new todo from the request body
+    const todo = req.body.todo;
     // 2. Push it into our todo array
+    todos.push(todo);
     // 3. Respond with the updated todos array
+    res.status(201).send({
+      data: [todo] // still send data in an array
+    })
   })
 
+// DELETE /todos/2
 router.route('/:index')
   .delete((req, res) => {
-    // 1. Grab the todo index from the query params.
+    // 1. Grab the todo index from the url params.
+    const { index } = req.params;
     // 2. Splice the old todo out of the todos array
+    const removedTodo = todos.splice(index, 1);
     // 3. Respond with the updated list of todos
+    res.status(202).send({
+      data: removedTodo
+    })
   })
 
 exports.router = router
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ShowTodos from './components/ShowTodos'
 import AddTodo from './components/AddTodo'
+import axios from 'axios';
 
 class TodoApp extends Component {
   state = {
@@ -8,21 +9,52 @@ class TodoApp extends Component {
     todos: []
   }
 
+  componentDidMount() {
+    this.refresh();
+  }
+
   clearInput = () => {
     this.setState({ todo: '' })
   }
 
-  addTodo = () => {
-    const nextTodos = Array.from(this.state.todos)
-    nextTodos.push(this.state.todo)
-    this.setState({ todos: nextTodos })
+  refresh = async () => {
+    try {
+      // 1. GET /todos to get all todos
+      const response = await axios.get('/todos');
+      // 2. Put todos in state
+      const todos = response.data.data;
+      this.setState({ todos });
+      console.log(response.data.data);
+      // code after will not run until the above is finished
+    }
+    catch (e) {
+      console.log(e);
+    }  
+  }
+
+  addTodo = async () => {
+    // Don't need these anymore
+    // const nextTodos = Array.from(this.state.todos)
+    // nextTodos.push(this.state.todo)
+    // this.setState({ todos: nextTodos })
+    try {
+      await axios.post('/todos',  {
+        todo: this.state.todo
+      })
+      this.refresh();
+    } catch(e) {
+      console.log(e);
+    } // finally is the third part try/catch
     this.clearInput()
   }
 
-  removeTodo = index => {
-    const nextTodos = Array.from(this.state.todos)
-    nextTodos.splice(index, 1)
-    this.setState({ todos: nextTodos })
+  removeTodo = async index => {
+    try {
+      await axios.delete(`/todos/${index}`);
+      this.refresh();
+    } catch(e) {
+      console.log(e);
+    }
   }
 
   handleChange = e => {
